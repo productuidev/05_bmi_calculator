497ff41dd44086e183d5ce5b7ab25c05f9670b1c
productuidev <productuidev@gmail.com>
2022-09-23 07:36:02 +0900

    reset

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function onSubmit(event) {
   const w = parseFloat(event.target[0].value)
   const h = parseFloat(event.target[1].value)
 
-  // console.log(w, h)
+  console.log(w, h)
 
   // 조건 : 숫자인지, 양수인지
   if(isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
@@ -12,5 +12,10 @@ function onSubmit(event) {
     return;
   }
 
+  // BMI는 소수점 2자리까지 표시
+  const bmi = w / (h*h)
+  console.log(bmi.toFixed(2))
 
+  const res = document.getElementById("res")
+  res.style.display = "block"
 }
