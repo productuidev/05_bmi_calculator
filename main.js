95cb71f7552669a4aed2a94842f375ed9934e05d
productuidev <productuidev@gmail.com>
2022-09-23 07:26:23 +0900

    condition

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-// submit
 function onSubmit(event) {
   event.preventDefault();
-  event.target[0].value
-  event.target[1].value
+
+  const w = parseFloat(event.target[0].value)
+  const h = parseFloat(event.target[1].value)
+
+  // console.log(w, h)
+
+  // 조건 : 숫자인지, 양수인지
+  if(isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+    alert("적절한 값이 아닙니다.")
+    return;
+  }
+
+
 }
