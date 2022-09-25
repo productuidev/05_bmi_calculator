dd61ebb5b93a5745f66c05f25e0173776e651991
productuidev <productuidev@gmail.com>
2022-09-26 01:43:37 +0900

    edit

--- main.js
+++ main.js
@@ -13,9 +13,26 @@ function onSubmit(event) {
   }
 
   // BMI는 소수점 2자리까지 표시
-  const bmi = w / (h*h)
+  const bmi = w / ((h/100) * (h/100))
   console.log(bmi.toFixed(2))
 
+  // result
   const res = document.getElementById("res")
   res.style.display = "block"
+
+  // bmi & state
+  document.getElementById("bmi").innerText = bmi.toFixed(2)
+
+  let state = ""
+
+  if(bmi < 18.5)
+    state = "저체중"
+  if(bmi >= 18.5 && bmi <= 24.9)
+    state = "정상"
+  if(bmi >= 25 && bmi <= 29.9)
+    state = "과체중"
+
+  document.getElementById("state").innerText = state
+
+
 }
